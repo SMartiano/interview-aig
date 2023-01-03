beec9f7ae9d7cb316b08e55706a70137ba951115
smartiano <samuel.martiano1@gmail.com>
2023-01-03 22:33:23 +0200

    added file with data to insert into the db

---
+++ DB/Data.sql
@@ -0,0 +1,100 @@
+insert into Category values ('general')
+
+insert into Category values ('business')
+
+insert into articles values ('With 97% of votes tallied, Netanyahu launches government talks',
+'Opposition leader taps long-time ally to hammer out coalition agreement posthaste, perhaps even before swearing-in of 25th Knesset on November 15, with focus on security and state-religion affairs',
+null,
+1)        
+insert into articles values ('Kanye West paid a settlement to a former employee over antisemitic rants in 2018, report says',
+'In the previously unreported 2018 settlement, the anonymous former employee alleged that West had praised Hitler and Nazis in business meetings.',
+null,
+1)        
+insert into articles values ('Israeli regional cooperation through music in Morocco',
+'While the festival has had Jewish and Israeli musicians in the past, this year, Israel’s Ministry of Regional Cooperation set aside funds to bring dozens of Israeli artists.',
+null,
+1)        
+insert into articles values ('Murchinson bids $995m for Nano Dimension - report',
+'The Canadian investment firm, the largest shareholder in the Israeli 3D printing company, has proposed buying all the shares, \''Bloomberg\'' reports.',
+'https://res.cloudinary.com/globes/image/upload/t_desktop_article_content_header_800%2A392/v1613885441/old/1465400.jpg',
+2)        
+insert into articles values ('E-commerce co Global-e lays off 5% of workforce',
+'The Israeli company is laying off 5% of its workforce - 40 out of 800 employees, mainly due to overlapping function in companies that have been acquired over the past year, including development roles in Global-e.',
+'https://res.cloudinary.com/globes/image/upload/t_desktop_article_content_header_800%2A392/v1615297070/direct/Global-e_Co-founders_Amir_Schlachet_Shahar_Tamari_Nir_Debbi_b86aaz.jpg',
+2)        
+insert into articles values ('Tel Aviv to add 600 homes in revamped Arlozorov Street',
+'In the plan there will be less room for cars and Arlozorov Street will be more pedestrian friendly with wider sidewalks and cycle paths alongside the light rail Purple Line.',
+'https://res.cloudinary.com/globes/image/upload/t_desktop_article_content_header_800%2A392/v1667731884/direct/b%D7%94%D7%93%D7%9E%D7%99%D7%94_%D7%9E%D7%95%D7%A6%D7%A2_3_qarp91.png',
+2)        
+insert into articles values ('Meta to lay off over 50 in Israel',
+'Facebook will make a small number of cuts in Israel and has no plans to cancel the lease it signed for 20 floors in the Landmark project in Tel Aviv.',
+'https://res.cloudinary.com/globes/image/upload/t_desktop_article_content_header_800%2A392/v1656337403/direct/shutterstock_2113535186_kldijc.jpg',
+2)        
+insert into articles values ('Israel''s forex reserves revalued higher',
+'Israel&rsquo;s foreign exchange reserves at the end of September 2022 stood at $185.997 billion, a decrease of $6.154 billion from their level at the end of August, the Bank of Israel reports.',
+'https://res.cloudinary.com/globes/image/upload/t_desktop_article_content_header_800%2A392/v1641997907/direct/shutterstock_158656643_wncjrh.jpg',
+2)        
+insert into articles values ('Naftali Bennett to incoming gov''t: Leftists should not be made to feel unwelcome',
+'In a statement on his Twitter, the former prime minister stressed the importance of respect and of serving the entire Israeli public, not just one sector.',
+null,
+1)        
+insert into articles values ('Palestinians to push for review of ''ongoing occupation'' at the Hague',
+'Amid a report that blamed Israel for war crimes in 2021 Gaza war, Palestinians to seek for ICC to rule that Israel''s alleged occupation is permanent and not temporary, potentially fueling anti-Israel sentiments',
+null,
+1)        
+insert into articles values ('Israeli tourism still far from pre-Covid records',
+'368,700 visitors entered Israel in October 2022, the Central Bureau of Statistics reports, down 24% from September 2019.',
+'https://res.cloudinary.com/globes/image/upload/t_desktop_article_content_header_800%2A392/v1651158139/direct/c6a0bcbd-cca6-40a0-b883-8ecb6eaeef0d_auuglv.jpg',
+2)        
+insert into articles values ('Ukraine: Russia is bringing ammunition disguised as humanitarian aid',
+'Ukraine has accused Russia of misusing humanitarian aid in order to safely reinforce its forces.',
+null,
+1)        
+insert into articles values ('30 new Magen David Adom smart AEDs insert into articles values (automated external defibrillator) will be placed in Jerusalem',
+'',
+null,
+1)        
+insert into articles values ('Paz sells 15% of Ashdod Oil Refinery to Shapir',
+'80% of the shares in Ashdod Oil Refinery will be distributed to Paz Oil shareholders, and the company will be listed on the Tel Aviv Stock Exchange.',
+'https://res.cloudinary.com/globes/image/upload/t_desktop_article_content_header_800%2A392/v1615686451/old/1219670.jpg',
+2)        
+insert into articles values ('Hackers claim to reveal identities of cops who arrested Mahsa Amini',
+'The 3ackd0or hacker group revealed what it said was the personal information and photos of the officers who beat Mahsa Amini.',
+null,
+1)        
+insert into articles values ('Attorney 1 backflips on approval of Lebanon agreement',
+'Attorney 1 Gali Baharav-Miara now says the agreement needs approval by the full government and to be laid before the Knesset.',
+'https://res.cloudinary.com/globes/image/upload/t_desktop_article_content_header_800%2A392/v1644318946/direct/644d1830-1321-4f05-8f35-eedbcc43a370_mgpjxg.jpg',
+2)        
+insert into articles values ('Here with my two Jewish lawyers'': New Trump book doesn''t spare his antisemitic quips',
+'Maggie Haberman’s new book ''Confidence Man'' shows how the 45th president never seemed to grow out of the mores of the 1980s, but he deflected charges of antisemitism by claiming he was the most pro-Israel president',
+'https://img.haarets.co.il/bs/00000183-a26e-d908-a3fb-fb7ea1670000/37/cd/d3e897124c3eaaf40878ade23da1/48829430.JPG?width=108&height=81',
+1)        
+insert into articles values ('It is time for all sectors in Israel to combat sexual abuse - editorial',
+'Living in a religious community does not afford one or one’s children protection from predators. All sectors of Israeli society need to address and combat sexual abuse.',
+null,
+1)               
+insert into articles values ('Lebanon will refuse a maritime security zone between Israel',
+'If both countries approve the maritime border agreement, it will be passed to the United Nations in order to be validated according to international law',
+'https://img.haarets.co.il/bs/00000183-a23b-d908-a3fb-fb3fff5c0000/e4/27/573e53e246fba7132c866fb6632c/31889.jpg?width=108&height=81',
+1)        
+insert into articles values ('My word: Calm or stormy waters ahead?',
+'When it comes to the maritime border dispute between Israel and Lebanon, the question can be legitimately asked in more than one sense. ',
+null,
+1)        
+insert into articles values ('Bank of Israel again hikes rate by 0.75%',
+'The Bank of Israel has raised interest rates for the fifth time this year. The Research Department raised its growth forecast for 2022 and kept its inflation forecast virtually unchanged.',
+'https://res.cloudinary.com/globes/image/upload/t_desktop_article_content_header_800%2A392/v1624964318/direct/%D7%AA%D7%9E%D7%95%D7%A0%D7%94-_3_gelv7i.jpg',
+2)        
+insert into articles values ('Shekel gains sharply as investors sell dollars',
+'Prico: Institutional investors are buying shekels to hedge their holdings and exporters are buying shekels to pay their employees and taxes.',
+'https://res.cloudinary.com/globes/image/upload/t_desktop_article_content_header_800%2A392/v1636298956/direct/shutterstock_2050002542_qtgd1t.jpg',
+2)        
+insert into articles values ('Ayelet Shaked was a right-wing star in Israeli politics... until she wasn''t',
+'Popularity Shaked garnered during her time as justice minister among those on the Right now seems like a distant dream, with polls indicating it''s better for lawmaker to quit politics to save face than run in November 1 elections',
+null,
+1)        
+insert into articles values ('On Sukkot, Israel needs to consider its environmental policies - editorial',
+'Israel needs to consider its policies on construction and land use, recreation, transport, energy, air quality, waste, avoiding marine pollution, protecting wildlife, etc.',
+null,
+1)
